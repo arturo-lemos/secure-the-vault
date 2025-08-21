0b7ffe415efcd7c3adfd996f9120589c06adc69d
Arturo <lemosart0987@gmail.com>
2025-08-20 21:34:47 -0500

    first commit assignment is done

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("code1");
+console.log("code1", 7 + 3);
+console.log("code2", (500 % 200) - 60); /*This one took me a minute*/
+console.log("code3", (300 % 200) - 61);
+console.log("message", `The vault has been secured. The combination is:`);
+const code1 = 10;
+const code2 = 40;
+const code3 = 39;
+console.log(code1 - code2 - code3);
+console.log("codeA", code1 - code2 - code3);
+console.log(`codeB ${code1 - code2 - code3}`);
+const message = "The vault has been secured. The combination is:";
+const codeA = -69;
+const codeB = -69;
+console.log(message, codeA, codeB);
+
+/*I did it this way but I believe I could have used let and possibly var. Let me know in your comments if you want me to do it another way.*/
